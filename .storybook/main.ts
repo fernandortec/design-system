8d8a5fb14bda9acbb83d68840b8babf59472c4f8
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-06-13 11:18:38 -0300

    :bug: (storybook) Fix: add subpath to storybook on prod

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,11 +7,18 @@ const config: StorybookConfig = {
 		"@storybook/addon-essentials",
 		"@chromatic-com/storybook",
 		"@storybook/addon-interactions",
-		"@storybook/addon-a11y"
+		"@storybook/addon-a11y",
 	],
 	framework: {
 		name: "@storybook/react-vite",
 		options: {},
+	},
+	viteFinal: (config, { configType }) => {
+		if (configType === "PRODUCTION") {
+			config.base = "/design-system/";
+		}
+
+		return config
 	},
 };
 export default config;
